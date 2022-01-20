e51375b08bb14d7da6ba0b66cbbd92906b433a2d
Moctavi <80235738+MeitenOctavio@users.noreply.github.com>
2022-01-20 21:41:17 +0800

    random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,20 @@
+import random
+
 def classy():
-  print("Keep it logically awesome.")
+  # print("Keep it logically awesome.") 
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  last= 13
+  # or can choose the next line. its same but diffrent expresion
+  # last= len(quotes)-1
+
+  rnd = random.randint(0,last)
+
+  # print(quotes[13])
+  print(quotes[rnd])
 
 if __name__== "__main__":
   classy()
